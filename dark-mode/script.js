de365974a0c30a59ea7c48c81140a13766c0f443
Hrithik C A <hrithikca@gmail.com>
2023-05-18 12:29:14 +0530

    added teal bg to middle p tag using class list

--- dark-mode/script.js
+++ dark-mode/script.js
@@ -1,5 +1,7 @@
 let body = document.querySelector('body').classList;
 let button = document.querySelector('button');
+let para = document.querySelectorAll('p');
+    para[1].classList.add('teal-bg');
 
 function toggleDarkMode(){
     body.toggle("dark-mode");
